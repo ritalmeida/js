ad454c2f27c258a4bd4e5876f54de2955e44f477
ritaalmeida <35456@ufp.edu.pt>
2025-09-13 16:24:32 +0100

    ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+//console.log('O console funcionou corretamente!')
+
+var vel = 78.2
+console.log('A velocidade do seu carro é ' + vel + 'Km/h')      //control+option+n para correr o código no MAC
+
+if (vel > 60) { //condição simples!
+    console.log('Você ultrupassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
