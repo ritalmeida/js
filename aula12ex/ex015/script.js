40d49472c0b0b256eafcf62b633116dfd156892f
ritaalmeida <35456@ufp.edu.pt>
2025-09-14 15:28:47 +0100

    ex15

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = window.document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'menino.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovem.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'homem.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'menina.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'rapariga.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulher.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+        
+    }
+}
