09ae3692d52ce4edbcb076d568221cc40b769d3a
ritaalmeida <35456@ufp.edu.pt>
2025-09-14 16:12:06 +0100

    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function gerarTabuada() {
+    let num = document.getElementById('num')
+    let tabuada = document.getElementById('tabuada')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tabuada.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tabuada.appendChild(item)
+        }
+    }
+}   
