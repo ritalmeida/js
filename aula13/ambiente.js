a0942c5c267820e8d17857755a0e0b28873bfd25
ritaalmeida <35456@ufp.edu.pt>
2025-09-14 15:38:14 +0100

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+var c = 1
+/*while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}*/
+
+/*for (c = 1 c <= 6; c++) {
+    console.log('Tudo bem?')
+}*/
+do {
+    console.log(`Passou ${c}`)
+    c++
+} while (c <= 6)
